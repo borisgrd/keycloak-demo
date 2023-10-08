a495d74021586258d4c3a4389c01f23cdd2037c4
borisgrd <borisgrd@gmail.com>
2023-10-07 20:31:53 -0400

    add jwt converter and lambda DSL to security conf

--- src/main/java/net/browse3d/keycloakdemo/SecurityConfig.java
+++ src/main/java/net/browse3d/keycloakdemo/SecurityConfig.java
@@ -1,16 +1,25 @@
 package net.browse3d.keycloakdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.crypto.factories.DefaultJWEDecrypterFactory;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-        // Old deprecated way
         // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
         // Exception {
         // http.csrf()
@@ -29,18 +38,19 @@ public class SecurityConfig {
 
         // }
 
+        // private final JwtDecoder jwtDecoder;
+        private final JwtAuthConverter jwtAuthConverter;
+
+        @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-                http.csrf()
-                                .disable()
-                                .authorizeHttpRequests()
-                                .anyRequest()
-                                .fullyAuthenticated();
-
-                http.oauth2ResourceServer()
-                                .jwt();
-
-                http.sessionManagement()
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                http
+                                .csrf(AbstractHttpConfigurer::disable)
+                                .authorizeHttpRequests(req -> req.anyRequest()
+                                                .fullyAuthenticated())
+                                .oauth2ResourceServer((oauth2ResourceServer) -> oauth2ResourceServer
+                                                .jwt((jwt) -> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
+                                .sessionManagement((session) -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
                 return http.build();
 
